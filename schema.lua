4a8ba2b00b1931e3fdc7dbf609da6410683cb185
Nguyen Dang Minh <nguyendangminh@gmail.com>
2020-04-21 15:48:29 +0700

    Clone IP Restriction plugin as-is

---
+++ schema.lua
@@ -0,0 +1,21 @@
+local typedefs = require "kong.db.schema.typedefs"
+
+
+return {
+  name = "ip-restriction",
+  fields = {
+    { protocols = typedefs.protocols_http },
+    { config = {
+        type = "record",
+        fields = {
+          { whitelist = { type = "array", elements = typedefs.cidr_v4, }, },
+          { blacklist = { type = "array", elements = typedefs.cidr_v4, }, },
+        },
+      },
+    },
+  },
+  entity_checks = {
+    { only_one_of = { "config.whitelist", "config.blacklist" }, },
+    { at_least_one_of = { "config.whitelist", "config.blacklist" }, },
+  },
+}
